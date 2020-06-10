7d1009707ccf8edc4d6399a4ed6e0ff28e0d7fe5
maikel abdelmalak <maikel22@hotmail.it>
2020-06-10 18:29:53 +0200

    .

--- main.js
+++ main.js
@@ -4,22 +4,33 @@ $.ajax({
     method : 'GET',
     success : function(data) {
         var mesi = {}
+        var venditori = {}
         for (var i = 0; i < data.length; i++) {
             var mese = moment(data[i].date, 'DD/MM/YYYY').format('MMMM')
             var vendite = data[i].amount;
+            var venditore = data[i].salesman;
             if(!mesi.hasOwnProperty(mese)) {
                 mesi[mese] = vendite;
             } else {
                 mesi[mese] += vendite;
            }
 
+            if(!mesi.hasOwnProperty(venditore)) {
+                venditori[venditore] = vendite;
+            } else {
+                venditori[venditore] += vendite;
+           }
+
         }
-        console.log(mesi);
+
         var etichette = Object.keys(mesi);
         var valori = Object.values(mesi);
+
+        var nomi = Object.keys(venditori);
+        var valori_2 = Object.values(venditori);
+
         var ctx = $('#myChart');
 
-        var ctx = document.getElementById('myChart');
         var myChart = new Chart(ctx, {
             type: 'line',
             data: {
@@ -54,6 +65,38 @@ $.ajax({
                         }
                     }]
                 }
+            }
+        });
+
+        var ctx_2 = $('#myChart-Doughnut');
+
+        var myChart_2 = new Chart(ctx_2, {
+            type: 'pie',
+            data: {
+                labels: nomi,
+                datasets: [{
+                    data: valori_2,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+
             }
         });
 
