fcf3f1f803fe48f065348e013420754d4f5c1ad5
maikel abdelmalak <maikel22@hotmail.it>
2020-06-11 16:00:51 +0200

    .

--- main.js
+++ main.js
@@ -3,102 +3,47 @@ $.ajax({
     url : "http://157.230.17.132:4001/sales",
     method : 'GET',
     success : function(data) {
-        var mesi = {}
+        var mesi = {
+            January: 0,
+            February: 0,
+            March: 0,
+            April: 0,
+            May: 0,
+            June: 0,
+            July: 0,
+            August: 0,
+            September: 0,
+            October: 0,
+            November: 0,
+            December: 0,
+        }
         var venditori = {}
         for (var i = 0; i < data.length; i++) {
 
-            var mese = moment(data[i].date, 'DD/MM/YYYY').format('MMMM')
-            var vendite = data[i].amount;
-            var venditore = data[i].salesman;
+            var mese_corrente = moment(data[i].date, 'DD/MM/YYYY').format('MMMM')
+            var vendita_corrente = data[i].amount;
+            var venditore_corrente = data[i].salesman;
 
-            if(!mesi.hasOwnProperty(mese)) {
-                mesi[mese] = vendite;
-            } else {
-                mesi[mese] += vendite;
-           }
+           mesi[mese_corrente] += vendita_corrente;
 
-            if(!venditori.hasOwnProperty(venditore)) {
-                venditori[venditore] = vendite;
+            if(!venditori.hasOwnProperty(venditore_corrente)) {
+                venditori[venditore_corrente] = vendita_corrente;
+
             } else {
-                venditori[venditore] += vendite;
+                venditori[venditore_corrente] += vendita_corrente;
            }
 
         }
 
         var etichette = Object.keys(mesi);
-        var valori = Object.values(mesi);
+        var valori_mesi = Object.values(mesi);
 
         var nomi = Object.keys(venditori);
-        var valori_2 = Object.values(venditori);
+        var valori_venditori = Object.values(venditori);
 
-        var ctx = $('#myChart');
-        var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: etichette,
-                datasets: [{
-                    label: '',
-                    data: valori,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
+        crea_line(etichette, valori_mesi)
 
-        var ctx_2 = $('#myChart-Doughnut');
-        var myChart_2 = new Chart(ctx_2, {
-            type: 'doughnut',
-            data: {
-                labels: nomi,
-                datasets: [{
-                    data: valori_2,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-
-            }
-        });
+        crea_doughnut(nomi, valori_venditori)
 
     },
     error : function () {
@@ -107,9 +52,69 @@ $.ajax({
 });
 
 
+function crea_line(label, data){
+    var ctx = $('#myChart');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: label,
+            datasets: [{
+                label: '',
+                data: data,
+                backgroundColor: [
+                    'rgba(255, 255, 255, 0)',
+                    // 'rgba(54, 162, 235, 1)',
+                    // 'rgba(255, 206, 86, 1)',
+                    // 'rgba(75, 192, 192, 1)',
+                    // 'rgba(153, 102, 255, 1)',
+                    // 'rgba(255, 159, 64, 1)'// 'rgba(255, 99, 132, 1)',
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 4
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
 
+function crea_doughnut(label, data){
+    var ctx_2 = $('#myChart-Doughnut');
+    var myChart_2 = new Chart(ctx_2, {
+        type: 'doughnut',
+        data: {
+            labels: label,
+            datasets: [{
+                data: data,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderColor: [
+                    'rgba(255, 255, 255, 1)',
 
+                ],
+                borderWidth: 3
+            }]
+        },
+        options: {
 
+        }
+    });
+}
 
 
 
