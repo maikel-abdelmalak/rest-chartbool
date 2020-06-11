5447d5cdbcdda88662ce11e6020ebf7cc0ae4923
maikel abdelmalak <maikel22@hotmail.it>
2020-06-11 18:13:11 +0200

    .

--- main.js
+++ main.js
@@ -1,118 +1,110 @@
 $('document').ready(function(){
-$.ajax({
-    url : "http://157.230.17.132:4001/sales",
-    method : 'GET',
-    success : function(data) {
-        var mesi = {
-            January: 0,
-            February: 0,
-            March: 0,
-            April: 0,
-            May: 0,
-            June: 0,
-            July: 0,
-            August: 0,
-            September: 0,
-            October: 0,
-            November: 0,
-            December: 0,
-        };
-        var venditori = {};
-        var vendite_totali = 0;
-        for (var i = 0; i < data.length; i++) {
-            vendite_totali += data[i].amount;
-            var mese_corrente = moment(data[i].date, 'DD/MM/YYYY').format('MMMM')
-            var vendita_corrente = data[i].amount;
-            var venditore_corrente = data[i].salesman;
+chiamata_get();
 
-           mesi[mese_corrente] += vendita_corrente;
-
-            if(!venditori.hasOwnProperty(venditore_corrente)) {
-                venditori[venditore_corrente] = vendita_corrente;
-
-            } else {
-                venditori[venditore_corrente] += vendita_corrente;
-           }
-
-        }
-
-        console.log(venditori);
-
-        var etichette = Object.keys(mesi);
-        var valori_mesi = Object.values(mesi);
-
-        var nomi = Object.keys(venditori);
-        var valori_venditori = Object.values(venditori);
-
-//rivedere
-        for (var nome_venditore in venditori) {
-           var importo_venditore = venditori[nome_venditore];
-           var percentuale_venditore = (importo_venditore * 100 / vendite_totali).toFixed(1);
-          venditori[nome_venditore] = percentuale_venditore;
-        }
-//fine rivedere
-         var select_1 = $('.nomi');
-         popola_select(nomi, select_1);
-
-         var select_2 = $('.mesi');
-         popola_select(etichette, select_2);
-
-         crea_line(etichette, valori_mesi);
-
-        crea_doughnut(nomi, valori_venditori);
-
-    },
-    error : function () {
-    alert("errore.");
-    }
-});
 
 $('button').click(function(){
 
     var dati = {
-        nome : $('.nomi option:selected').text(),
-        mese :$('.mesi option:selected').text(),
-        valore_vendita : parseInt($('input').val())
+        salesman : $('.nomi option:selected').text(),
+        amount: parseInt($('input').val()),
+        date: $('.mesi option:selected').text()
     }
+    dati.date = moment(dati.date, 'MMMM').format('01/MM/2017');
+    console.log(dati.date);
+    if(dati.salesman != '-Scegli venditore-' && dati.date != '-Scegli mese vendita-' && !isNaN(dati.amount)){
 
-    var nome = $('.nomi option:selected').text();
-    var mese = $('.mesi option:selected').text();
-    var valore_vendita = parseInt($('input').val());
-
-    if(nome != '-Scegli venditore-' && mese != '-Scegli mese vendita-' && !isNaN(valore_vendita)){
         $.ajax({
             url : "http://157.230.17.132:4001/sales",
             method : 'POST',
             data: dati,
             success : function(dati) {
+            console.log(dati);
 
-                window.mychar.update()
-                window.mychar_2.update()
-
+            chiamata_get();
 
             },
             error : function () {
             alert("errore.");
             }
         });
-
     }else{
         alert('hai inserito valori non validi')
     }
 
-console.log(dati);
-
-
-
-
-
-
 })
 
+function chiamata_get(){
+    $.ajax({
+        url : "http://157.230.17.132:4001/sales",
+        method : 'GET',
+        success : function(data) {
+            var mesi = {
+                January: 0,
+                February: 0,
+                March: 0,
+                April: 0,
+                May: 0,
+                June: 0,
+                July: 0,
+                August: 0,
+                September: 0,
+                October: 0,
+                November: 0,
+                December: 0,
+            };
+            var venditori = {};
+            var vendite_totali = 0;
+            for (var i = 0; i < data.length; i++) {
+                var mese_corrente = moment(data[i].date, 'DD/MM/YYYY').format('MMMM')
+                var vendita_corrente = parseInt(data[i].amount);
+                var venditore_corrente = data[i].salesman;
+                vendite_totali += vendita_corrente;
+               mesi[mese_corrente] += vendita_corrente;
+
+               mesi[mese_corrente] += vendita_corrente;
+
+                if(!venditori.hasOwnProperty(venditore_corrente)) {
+                    venditori[venditore_corrente] = vendita_corrente;
+
+                } else {
+                    venditori[venditore_corrente] += vendita_corrente;
+               }
+
+            }
+
+            var etichette = Object.keys(mesi);
+            var valori_mesi = Object.values(mesi);
+
+            var nomi = Object.keys(venditori);
+            var valori_venditori = Object.values(venditori);
+
+    //rivedere
+            for (var nome_venditore in venditori) {
+               var importo_venditore = venditori[nome_venditore];
+               var percentuale_venditore = (importo_venditore * 100 / vendite_totali).toFixed(1);
+              venditori[nome_venditore] = percentuale_venditore;
+            }
+    //fine rivedere
+             var select_1 = $('.nomi');
+             popola_select(nomi, select_1);
+
+             var select_2 = $('.mesi');
+             popola_select(etichette, select_2);
+
+             crea_line(etichette, valori_mesi);
+
+            crea_doughnut(nomi, valori_venditori);
+
+        },
+        error : function () {
+        alert("errore.");
+        }
+    });
+}
 
 function crea_line(label, data){
     var ctx = $('#myChart');
-    var myChart = new Chart(ctx, {
+     myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: label,
